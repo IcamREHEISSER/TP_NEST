f92521b229ce112fa846892978a6f3119644a835
MatREHEISSER <mathias.reheisser@icam.fr>
2023-11-22 09:58:59 +0100

    Ajout dashboard de base

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,14 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('pages/index.hbs')
+  root() {
+    return { message: 'hello world!' };
+  }
+
+  @Get('index.html')
+  @Render('pages/index.hbs')
+  index() {
+    return { message: 'hello world!' };
   }
 }
